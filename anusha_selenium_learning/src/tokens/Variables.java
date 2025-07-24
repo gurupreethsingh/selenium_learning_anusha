741d2423194cc830d0d51f83d9bb4c77bdc0e2c3
gurupreethsingh <igurupreeth@gmail.com>
2025-07-24 15:40:36 +0530

    updates

--- anusha_selenium_learning/src/tokens/Variables.java
+++ anusha_selenium_learning/src/tokens/Variables.java
@@ -4,6 +4,7 @@ public class Variables {
 
 	public static void main(String[] args) 
 	{
+		short s =  23; 
 		int a = 10;
 		float f  = 5.4f; 
 		double d = 3.64564;
@@ -14,5 +15,7 @@ public class Variables {
 		System.out.println(a);
 		System.out.println(f);
 		System.out.println(d);
+		System.out.println(s);
+		System.out.println(l);
 	}
 }
