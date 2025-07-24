01faa431eb70297c615a89ac17a19394b68f07de
gurupreethsingh <igurupreeth@gmail.com>
2025-07-24 14:52:09 +0530

    Java selenium project started, i have added variable code.

---
+++ anusha_selenium_learning/src/tokens/Variables.java
@@ -0,0 +1,14 @@
+package tokens;
+
+public class Variables {
+
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		float f  = 5.4f; 
+		double d = 3.64564;
+		System.out.println(a);
+		System.out.println(f);
+		System.out.println(d);
+	}
+}
